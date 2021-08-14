be79d786b20e0b210082a44cf46dab81b39e9564
abdallaSuliman <abdullhazuber@gmail.com>
2021-08-14 20:11:02 +0800

    add checkout components

---
+++ src/Checkout.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Checkout.css'
+
+function Checkout() {
+    return (
+        <div className="chekout">
+            <div className="checkout__left">
+                <h1 className="checkout__title">
+                    Your Shopping Baket
+                </h1>
+                <div className="checkout__items">
+                    {/* items */}
+                    {/* items */}
+                    {/* items */}
+                    {/* items */}
+                </div>
+            </div>
+            <div className="checkout__right">
+                <h1>Subtotal will go here</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Checkout
+
--- src/Header.js
+++ src/Header.js
@@ -2,11 +2,14 @@ import React from 'react'
 import SearchIcon from '@material-ui/icons/Search'
 import { ShoppingBasket } from '@material-ui/icons'
 import './Header.css'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
     return (
         <div className="header">
+            <Link to='/'>
             <img className="header__logo" src='https://www.freepnglogos.com/uploads/amazon-png-logo-vector/world-brand-amazon-png-logo-vector-27.png' alt="logo"></img>
+            </Link>
             <div className="header__search">
                 <input className="header__searchInput" type="text"></input>
                 <SearchIcon className="header__searchIcon"/>
@@ -24,10 +27,12 @@ export default function Header() {
                         <span className="optionLineOne">Your</span>
                         <span className="optionLineTwo">Prime</span>
                     </div>
+                    <Link to='/checkout'>
                     <div className="header__opionBasket">
                         <ShoppingBasket />
                         <span className="optionLineTwo header__basketCount">0</span>
                     </div>
+                    </Link>
                 </div>
         </div>
     )
